3d32fa5d7af1143eeb76b0957c29b5755c4e29e1
Sam Bashton <sam@bashton.com>
2015-06-02 11:43:41 +0100

    Fix template spec for client config

--- manifests/clientconfig.pp
+++ manifests/clientconfig.pp
@@ -4,10 +4,10 @@ define openvpn::clientconfig ($config = []) {
   validate_array($config)
 
   file { "/etc/openvpn/ccd/${name}":
-    owner    => 'root',
-    group    => 'root',
-    mode     => '0644',
-    template => template('openvpn/clientconfig.erb'),
-    require  => File['/etc/openvpn/ccd'],
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('openvpn/clientconfig.erb'),
+    require => File['/etc/openvpn/ccd'],
   }
 }
